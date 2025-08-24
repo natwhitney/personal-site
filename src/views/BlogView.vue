2dbfd2dfb906564d14d0ae886f88b826ad7c7a60
FranticNine <franticnine2015@gmail.com>
2025-08-23 23:05:18 -0500

    Got router working for blog posts

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
 import PostCard from '@/components/PostCard.vue'
 import { useBlogStore } from '@/stores/blog'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const store = useBlogStore()
 store.getBlogPosts()
+
+// const computedKey = computed(() => {
+//   return (
+//     blogPost.date.toLocaleString(undefined, { day: 'numeric', month: 'numeric', year: 'numeric' }) +
+//     '/' +
+//     blogPost.title
+//   )
+// })
 </script>
 
 <template>
-  <div v-for="blogPost in store.blogPosts" :key="blogPost.title">
+  <div
+    v-for="blogPost in store.blogPosts"
+    :key="blogPost.title"
+    v-on:click="
+      router.push(
+        `blog/${blogPost.date.toLocaleString(undefined, { day: 'numeric', month: 'numeric', year: 'numeric' })}/${blogPost.title.replaceAll(' ', '-')}`,
+      )
+    "
+  >
     <post-card
       :title="blogPost.title"
       :date="blogPost.date"
